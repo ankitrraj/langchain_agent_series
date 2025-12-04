8ee6735c5f9fe2e3fe0e3f8a402fa8af04596d71
ankitrraj <ankitrajoria81@gmail.com>
2025-12-04 15:15:08 +0530

    mainagent update laso wroking condition

--- groq/mainagent.py
+++ groq/mainagent.py
@@ -1,65 +1,25 @@
 from langchain_groq import ChatGroq
-from langchain.agents import initialize_agent, AgentType
-from langchain.tools import Tool
-
-from langchain import hub  # ✅ Add this
-
-
-import os 
+import os
+from langchain.agents import create_agent
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# define tool
-
-def calculator(expression: str) -> str:
-    """Solves math expressions"""
-    try:
-        return str(eval(expression))
-    except:
-        return "Invalid expression"
-    
-tools=[
-    Tool(
-        name ="calcu",
-        func= calculator,
-        description = "usefull for solve math equation "
-
-    )
-]
-
-# hub
-prompt = hub.pull("hwchase17/react")
-
-
-# define llm
 llm = ChatGroq(
     model="llama-3.1-8b-instant",
-    groq_api_key=os.getenv("GROQ_API_KEY")  # ✅ Added API key
-
-    
+    temperature=0.3,
+    groq_api_key=os.getenv("GROQ_API_KEY")
 )
-#  create agent
-agent = initialize_agent(
-    llm=llm,
-    tools = tools,
-    promt = prompt,
-    agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
 
+agent = create_agent(
+    model=llm,
+    system_prompt="you are a customer support agent from withcaring"
 )
-# agent_execute = AgentExecutor(
-#     agent =agent,
-#     tools=tools
-# )
-result =agent.invoke({
-    "input": "what is 2+2=?"
+
+result = agent.invoke({
+    "messages": [
+        {"role": "user", "content": "please give me the contact to sales support"}
+    ]
 })
 
-print(f"result is = {result}")
-
-
-
-
-    
-
-
+print("\nResult:", result["messages"][-1].content)
